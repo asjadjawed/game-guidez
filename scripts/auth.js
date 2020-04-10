a2942a0d763b3e5802306031e2c0c7a0fb8972bc
Asjad Jawed Siddiqui Qadri <asjadjawed@gmail.com>
2020-04-10 15:15:17 +0500

    Login and Logout

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,5 +1,4 @@
 //signup
-
 const signupForm = document.querySelector("#signup-form");
 signupForm.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -7,9 +6,30 @@ signupForm.addEventListener("submit", (event) => {
   const password = signupForm["signup-password"].value;
 
   auth.createUserWithEmailAndPassword(email, password).then((cred) => {
-    console.log(cred.user);
     const modal = document.querySelector("#modal-signup");
     M.Modal.getInstance(modal).close();
     signupForm.reset();
+  });
+});
+
+// logout
+const logout = document.querySelector("#logout");
+logout.addEventListener("click", (e) => {
+  e.preventDefault();
+  auth.signOut().then(() => console.log("User signed out."));
+});
+
+//login
+const loginForm = document.querySelector("#login-form");
+loginForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const email = loginForm["login-email"].value;
+  const password = loginForm["login-password"].value;
+
+  auth.signInWithEmailAndPassword(email, password).then((cred) => {
+    console.log(cred.user);
+    const modal = document.querySelector("#modal-login");
+    M.Modal.getInstance(modal).close();
+    loginForm.reset();
   });
 });
